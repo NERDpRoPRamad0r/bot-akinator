bf432edcf1649a3ef96473eea68b1bea86b5fa10
NERDpRoPRamad0r <graziele.balbo@gmail.com>
2026-08-17 13:02:15 -0300

    Add Discord bot command to save and process images

---
+++ akinator.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from codigo_ai import ai
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def salvar(ctx):
+    lista = ctx.message.attachments
+    for arquivo in lista:
+        nome = arquivo.filename
+        await arquivo.save(f"imagens/{nome}")
+
+        arquivo = ai("keras_model.h5",
+                    "label.txt",
+                    f"imagens/{nome}"
+                    )
+
+        await ctx.send(f"eu acho que é: {nome}")
+
+        
+
+bot.run("MEU TOKEN")
